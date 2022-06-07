a9261111ad56c85da0e8f2f07bf7e5bd1ba96b4e
Hriday <kannoujia.hariday@gmail.com>
2022-06-07 11:48:47 +0530

    application startup file added

--- spring/src/main/java/com/app/spring/Application.java
+++ spring/src/main/java/com/app/spring/Application.java
@@ -1,13 +1,17 @@
 package com.app.spring;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	  ApplicationContext ctx = 	SpringApplication.run(Application.class, args);
+	  Arrays.stream(ctx.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
